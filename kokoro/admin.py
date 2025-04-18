adc465147d87e638e229859ee41fc8180c951667
Tiziano <tiziano.dallavilla@grupposandonato.it>
2025-04-18 11:58:08 +0200

    create class Patient_profiles

--- kokoro/admin.py
+++ kokoro/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from .models import Patient_profile
 
-# Register your models here.
-# Salve
+class Patient_profileAdmin(admin.ModelAdmin):
+    # "list_display": definisce le colonne visibili nella lista pazienti. Mostra cognome, nome, sesso e data di nascita.
+	list_display = ("last_name", "first_name", "sex", "date_of_birth")
+
+
+admin.site.register(Patient_profile, Patient_profileAdmin)
--- kokoro/models.py
+++ kokoro/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+import uuid
+import datetime
+from dateutil.relativedelta import relativedelta
 
-# Create your models here.
+# "Patient" eredita da "models.Model", quindi sarà mappato in una tabella del database.
+class Patient_profile(models.Model):
+	# UUIDField: campo che contiene un UUID (Universal Unique Identifier).
+	id = models.UUIDField(
+		# "primary_key=True": indica che questo è il campo identificativo unico della tabella.
+		primary_key = True,
+		# "default=uuid.uuid4": genera un nuovo UUID ogni volta che viene creato un oggetto.
+		default = uuid.uuid4,
+		# "editable=False": impedisce la modifica del campo tramite interfaccia admin o form.
+		editable = False
+		)
+	
+	# TextChoices: modo elegante per definire scelte enumerate leggibili dall'utente.
+	class Sex(models.TextChoices):
+		# M e F sono valori memorizzati nel DB.
+		# "Male" e "Female" sono le etichette leggibili.
+		MAN = "M", "Male"
+		WOMAN = "F", "Female"
+			
+	# CharField: campo stringa, con lunghezza massima di 100 caratteri.        
+	last_name = models.CharField(max_length=100)
+	first_name = models.CharField(max_length=100)
+	
+	# choices=Sex: obbliga l’utente a selezionare uno dei valori definiti nella classe Sex.
+	sex = models.CharField(
+		max_length=1,
+		choices=Sex
+		)
+	date_of_birth = models.DateField()
+	nation = models.CharField(max_length=100, blank=True, default='')
+	region = models.CharField(max_length=100, blank=True, default='')
+	province = models.CharField(max_length=100, blank=True, default='')
+	height = models.PositiveIntegerField(null=True, blank=True)
+	weight = models.PositiveIntegerField(null=True, blank=True)
+	cardioref_id = models.CharField(max_length=100, blank=True, default='')
+
+	class Meta:
+		# permissions: aggiunge un permesso personalizzato che potrà essere usato per controllare l’accesso a dati sensibili.
+		permissions = [
+		("access_sensible_info", "Can view patient sensible info")
+		]
+
+	def __str__(self):
+		return f'{self.id}'
