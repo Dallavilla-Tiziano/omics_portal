e949d7bcd2bd52a3e6b326957a509024cf38d699
saraparatico <sara.paratico@gmail.com>
2025-04-24 14:04:15 +0200

    admin modifications

--- kokoro/admin.py
+++ kokoro/admin.py
@@ -1,34 +1,32 @@
 from django.contrib import admin
 from .models import Patient_profile, Clinical_Status, Clinical_evaluation, Comorbidities
 
-class Patient_profileAdmin(admin.ModelAdmin):
-    # "list_display": definisce le colonne visibili nella lista pazienti. Mostra cognome, nome, sesso e data di nascita.
-	list_display = ("last_name", "first_name", "sex", "date_of_birth")
-	
-admin.site.register(Patient_profile, Patient_profileAdmin)
 
-# !!! Siccome vorrei che Clinical_evaluation fosse una sottoclasse di Clinical_Status,
-#     ho provato a gestire la situazione con "interlines", ma non so se è il modo più corretto 
-class ClinicalEvaluationInline(admin.TabularInline):
-    model = Clinical_evaluation
-    extra = 1
-
-class ComorbiditiesInline(admin.TabularInline):
-    model = Clinical_evaluation
-    extra = 1
-
-admin.site.register(Clinical_Status)
-class ClinicalStatusAdmin(admin.ModelAdmin):
-    inlines = [ClinicalEvaluationInline, ComorbiditiesInline]
-
-admin.site.register(Clinical_evaluation)
 class ClinicalEvaluationAdmin(admin.ModelAdmin):
     list_display = ['date_of_visit', 'EvaluationPreATC', 'symptoms', 'SVT', 'atrial_fibrillation', 'flutter',
                     'atrial_tachycardia', 'paroxysmal_supraventricular_tachycardia', 'wolff_parkinson_white', 
                     'besv', 'bev', 'premature_ventricular_contraction', 'thrombosis']
-    
+class ClinicalEvaluationInline(admin.TabularInline):
+    model = Clinical_evaluation
+    extra = 1
 
-admin.site.register(Comorbidities)
 class ComorbiditiesAdmin(admin.ModelAdmin):
     list_display = ['arterial_hypertension']
+class ComorbiditiesInline(admin.TabularInline):
+    model = Clinical_evaluation
+    extra = 1
+
+
+class Patient_profileAdmin(admin.ModelAdmin):
+     inlines = [
+		ClinicalEvaluationInline,
+		ComorbiditiesInline,
+	]
+     # "list_display": definisce le colonne visibili nella lista pazienti. Mostra cognome, nome, sesso e data di nascita.
+     list_display = ("last_name", "first_name", "sex", "date_of_birth")
+
+
+admin.site.register(Patient_profile, Patient_profileAdmin)
+admin.site.register(Clinical_evaluation)
+admin.site.register(Comorbidities)
 
