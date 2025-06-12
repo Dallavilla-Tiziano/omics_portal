834ecf39f590012652687c8023aa46d001d7ee66
saraparatico <sara.paratico@gmail.com>
2025-06-12 17:27:49 +0200

    first changes in _base.html

--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,8 @@ from django.db.models import Count
 from collections import Counter
 import json
 from patients.models import Patient, Sample, Analysis
+### NEW ###
+from kokoro.models import PatientProfile, PatientStudy 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
@@ -22,24 +24,35 @@ class HomePageView(TemplateView):
         total_samples = Sample.objects.count()
         total_analysis = Analysis.objects.count()
 
+        ### NEW ###
+        total_patientProfiles = PatientProfile.objects.count()
+
         # Get distribution data
         nations = Patient.objects.values_list("nation", flat=True)
         sex_counts = Patient.objects.values_list("sex", flat=True)
         dob_years = Patient.objects.values_list("date_of_birth", flat=True)
+
+        ### NEW ###
+        sexProfiles_counts = Patient.objects.values_list("sex", flat=True)
 
         # Process distributions
         nation_distribution = dict(Counter(nations))
         sex_distribution = dict(Counter(sex_counts))
         dob_distribution = dict(Counter(d.year for d in dob_years if d))  # Group by birth year
 
+        ### NEW ###
+        sexProfiles_distribution = dict(Counter(sexProfiles_counts))
+
         # Add data to context
         context.update({
             "total_patients": total_patients,
             "total_samples": total_samples,
             "total_analysis": total_analysis,
+            "total_kokoroPatients": total_patientProfiles,
             "nation_distribution": json.dumps(nation_distribution),
             "sex_distribution": json.dumps(sex_distribution),
             "dob_distribution": json.dumps(dob_distribution),
+            "sex_kokoroDistribution": json.dumps(sexProfiles_distribution),
         })
 
         return context
