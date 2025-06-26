f726a86d4961d6b138e45988217ad72a8b54b996
Tiziano <tiziano.dallavilla@grupposandonato.it>
2025-06-26 16:18:07 +0200

    Adds Study and Late Potential submissions
    Enables users to submit and edit Study and Late Potential data.
    
    Introduces new views and URLs for handling Study and Late Potential forms,
    integrating them into the submission workflow.

--- submissions/urls.py
+++ submissions/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import PatientProfileCreateView, PatientProfileUpdateView
+from .views import (PatientProfileCreateView, PatientProfileUpdateView, LatePotentialCreateView,
+					LatePotentialUpdateView, StudyCreateView, StudyUpdateView
+				)
 
 app_name = 'submissions'
 
 urlpatterns = [
+	path('study/add/', StudyCreateView.as_view(), name='add_studyl'),
+    path('study/edit/<uuid:pk>/', StudyUpdateView.as_view(), name='edit_study'),
+	path('latepotential/add/', LatePotentialCreateView.as_view(), name='add_latepotential'),
+    path('latepotential/edit/<uuid:pk>/', LatePotentialUpdateView.as_view(), name='edit_latepotential'),
 	path('patientprofile/add/', PatientProfileCreateView.as_view(), name='add_patientprofile'),
     path('patientprofile/edit/<uuid:pk>/', PatientProfileUpdateView.as_view(), name='edit_patientprofile'),
 	# Optional success page after submission
--- submissions/views.py
+++ submissions/views.py
@@ -2,9 +2,59 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
-from kokoro.models import PatientProfile
-from kokoro.forms import PatientProfileForm
+from kokoro.models import (
+						PatientProfile, Late_potentials, Study,
+						Ablation, Adrenaline_test, Ajmaline_test,
+						Clinical_evaluation, ClinicalEvent, CoronaryIntervention,
+						DeviceEvent, DeviceImplant, DeviceInstance,
+						EP_study, ECG, ECHO,
+						Flecainide_test, Genetic_profile, Genetic_status,
+						Genetic_test, RMN_TC_PH, Sample,
+						ValveIntervention,
+					)
+from kokoro.forms import (
+						PatientProfileForm, LatePotentialForm, StudyForm,
+						AblationForm, AdrenalineTestForm, AjmalineTestForm,
+						Clinical_evaluationForm, ClinicalEventForm, CoronaryInterventionForm,
+						DeviceEventForm, DeviceImplantForm, DeviceInstanceForm,
+						EP_studyForm, ECGForm, ECHOForm,
+						Flecainide_testForm, Genetic_profileForm, Genetic_statusForm,
+						Genetic_testForm, RMN_TC_PHTest, SampleForm,
+						ValveInterventionForm,
+					)
 
+
+class StudyCreateView(LoginRequiredMixin, CreateView):
+    model = Study
+    form_class = StudyForm
+    template_name = 'submissions/study_form.html'
+    success_url = reverse_lazy('submissions:submission_success')
+
+class StudyUpdateView(LoginRequiredMixin, UpdateView):
+    model = Study
+    form_class = StudyForm
+    template_name = 'submissions/study_form.html'
+    success_url = reverse_lazy('submissions:submission_success')
+
+    def get_queryset(self):
+        # Optional: limit which profiles a user can edit
+        return Late_potentials.objects.all()
+
+class LatePotentialCreateView(LoginRequiredMixin, CreateView):
+    model = Late_potentials
+    form_class = LatePotentialForm
+    template_name = 'submissions/latepotential_form.html'
+    success_url = reverse_lazy('submissions:submission_success')
+
+class LatePotentialUpdateView(LoginRequiredMixin, UpdateView):
+    model = Late_potentials
+    form_class = LatePotentialForm
+    template_name = 'submissions/latepotential_form.html'
+    success_url = reverse_lazy('submissions:submission_success')
+
+    def get_queryset(self):
+        # Optional: limit which profiles a user can edit
+        return Late_potentials.objects.all()
 
 class PatientProfileCreateView(LoginRequiredMixin, CreateView):
     model = PatientProfile
